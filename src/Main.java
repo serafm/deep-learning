6b231a87136daeab4acc445905ef584cc0fa9557
serafm <cs04555@uoi.gr>
2023-09-24 15:59:52 +0300

    [UPDATE] Updated(finished) LoadDataset class.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        MLP mlp = new MLP(1,3,1,1,1,"relu");
+        mlp.LoadDataset(Path.of("data/train.csv"));
+        ArrayList<ArrayList<String>> train = mlp.getData();
+
+        mlp.LoadDataset(Path.of("data/test.csv"));
+        ArrayList<ArrayList<String>> test = mlp.getData();
+
+        System.out.println("Train dataset line 1: " + train.get(0));
+        System.out.println("Test dataset line 1: " + test.get(0));
+    }
+}
